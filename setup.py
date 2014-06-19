4e846f55e23a5cb094faa0892702f46bf8ce793a
Raihan Kibria <raihan@kibria.de>
2014-06-19 18:46:05 +0200

    #7

--- setup.py
+++ setup.py
@@ -3,24 +3,26 @@ from os.path import dirname, join
 from distutils.core import setup
 from distutils.extension import Extension
 try:
-    from Cython.Distutils import build_ext
-    have_cython = True
+        from Cython.Build import cythonize
+        have_cython = True
 except ImportError:
-    have_cython = False
+        have_cython = False
 
 if have_cython:
-    ext = Extension('fixpoint', ['yapyg/fixpoint.pyx'])
+        ext = cythonize('yapyg/fixpoint.pyx')
 else:
-    ext = Extension('fixpoint', ['yapyg/fixpoint.c'],
-        extra_compile_args=['-std=c99', '-ffast-math', '-fPIC'])
+        ext = [
+                Extension('fixpoint', ['yapyg/fixpoint.c'],
+                        extra_compile_args=['-std=c99', '-ffast-math', '-fPIC']),
+                ]
 
 setup(
-    name='yapyg',
-    version='0.1.0',
-    description='Yet Another Python Game Engine',
-    author='Raihan Kibria',
-    author_email='raihan@kibria.de',
-    url='https://github.com/rkibria/yapyg',
-    packages=['yapyg'],
-    ext_modules=[ext]
-    )
+        name='yapyg',
+        version='0.1.0',
+        description='Yet Another Python Game Engine',
+        author='Raihan Kibria',
+        author_email='raihan@kibria.de',
+        url='https://github.com/rkibria/yapyg',
+        packages=['yapyg', 'yapyg.helpers', 'yapyg.movers', 'yapyg.viewers'],
+        ext_modules=ext
+        )
