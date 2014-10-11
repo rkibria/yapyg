52e97fb3a272d3d8eb183084e59e13acf880c8ef
Raihan Kibria <raihan@kibria.de>
2014-10-11 23:02:19 +0100

    Fix for setup without cython

--- setup.py
+++ setup.py
@@ -11,16 +11,21 @@ except ImportError:
 if have_cython:
         ext = cythonize("yapyg/*.pyx") + cythonize("yapyg_movers/*.pyx") + cythonize("yapyg_widgets/*.pyx")
 else:
-        ext = [ Extension('yapyg/collisions', ['yapyg/collisions.c']),
+        ext = [
+                Extension('yapyg/collisions', ['yapyg/collisions.c']),
+                Extension('yapyg/controls', ['yapyg/controls.c']),
                 Extension('yapyg/entities', ['yapyg/entities.c']),
                 Extension('yapyg/fixpoint', ['yapyg/fixpoint.c']),
-                Extension('yapyg/fixpoint_trig', ['yapyg/fixpoint_trig.c']),
                 Extension('yapyg/fixpoint_2d', ['yapyg/fixpoint_2d.c']),
                 Extension('yapyg/fixpoint_coll', ['yapyg/fixpoint_2d.c']),
+                Extension('yapyg/fixpoint_trig', ['yapyg/fixpoint_trig.c']),
+                Extension('yapyg/globals', ['yapyg/globals.c']),
                 Extension('yapyg/movers', ['yapyg/movers.c']),
+                Extension('yapyg/screen', ['yapyg/screen.c']),
                 Extension('yapyg/sprites', ['yapyg/sprites.c']),
                 Extension('yapyg/texture_db', ['yapyg/texture_db.c']),
                 Extension('yapyg/tiles', ['yapyg/tiles.c']),
+                Extension('yapyg/view', ['yapyg/view.c']),
                 Extension('yapyg_movers/physical', ['yapyg_movers/physical.c']),
                 Extension('yapyg_movers/linear', ['yapyg_movers/linear.c']),
                 Extension('yapyg_widgets/display_widget', ['yapyg_widgets/display_widget.c']),
